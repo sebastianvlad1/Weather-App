9991dfd486846cc140e15b53f4e7aafe68b56a47
sebastianvlad1 <sebastianbranzas@gmail.com>
2019-06-21 15:15:46 +0300

    Remove unnecessary code in js file

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-//console.log('Client side js file is loaded!');
-
-/*
-fetch('http://localhost:3000/weather/?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    });
-});*/
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
