adcaa1aae331965026a2b7b74e005d024964c4fa
sebastianvlad1 <sebastianbranzas@gmail.com>
2019-06-24 20:00:49 +0300

    Forecast info edited.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location!', undefined);
         } else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% change of rain.');
+            console.log(body.daily.data[0]);
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% change of rain. The high today is ' + body.daily.data[0].temperatureMax + ', with a low of ' + body.daily.data[0].temperatureMin + '.');
         }
     });
 };
