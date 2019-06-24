ac1ce2b8e3653123f027b4ff94050b880f264ca6
sebastianvlad1 <sebastianbranzas@gmail.com>
2019-06-24 19:51:46 +0300

    Edit precipitations

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location!', undefined);
         } else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' change of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% change of rain.');
         }
     });
 };
